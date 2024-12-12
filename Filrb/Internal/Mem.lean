9916d583b812e2670c94a4c1ad31e23822c1a333
Henrik Böving <hargonix@gmail.com>
2024-12-12 10:15:44 +0100

    doc: Raw.Mem

--- Filrb/Internal/Mem.lean
+++ Filrb/Internal/Mem.lean
@@ -1,9 +1,17 @@
 import Filrb.Internal.Raw
 
+/-!
+This module defines the notion of membership in a raw red black tree and proves that `Raw.contains`
+corresponds to this notion.
+-/
+
 namespace Filrb
 namespace Internal
 namespace Raw
 
+/--
+`x` is a member of a red black tree.
+-/
 inductive Mem (x : α) : Raw α → Prop where
   | here : Mem x (.node left x color right)
   | left (hleft : Mem x left) : Mem x (.node left y color right)
@@ -15,6 +23,9 @@ instance : Membership α (Raw α) where
 theorem mem_of_contains_eq_true {x : α} {t : Raw α} (h : t.contains x = true) : x ∈ t := sorry
 theorem contains_eq_true_of_mem {x : α} {t : Raw α} (h : x ∈ t) : t.contains x = true := sorry
 
+/--
+`contains` decides `Mem`.
+-/
 instance {x : α} {t : Raw α} : Decidable (x ∈ t) :=
   decidable_of_decidable_of_iff (Iff.intro mem_of_contains_eq_true contains_eq_true_of_mem)
 
