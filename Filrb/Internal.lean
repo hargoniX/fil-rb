0179c1f4ea3f8582a60bbe470370f940639fd7f1
Henrik Böving <hargonix@gmail.com>
2024-12-12 10:09:49 +0100

    doc: document Internal.Raw

--- Filrb/Internal.lean
+++ Filrb/Internal.lean
@@ -1,3 +1,8 @@
 import Filrb.Internal.Raw
 import Filrb.Internal.Invariants
 import Filrb.Internal.Mem
+
+/-!
+This module contains the internal implementation details of the red black tree implementation
+and thus has no stability guarantees to the outside world.
+-/
