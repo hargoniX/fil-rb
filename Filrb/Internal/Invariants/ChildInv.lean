3a84bcf3b7f41d9773e8eb6c389d4cb944d37688
Lingyin Luo <Luo.Lingyin@campus.lmu.de>
2025-01-26 12:39:46 +0100

    error fix

--- Filrb/Internal/Invariants/ChildInv.lean
+++ Filrb/Internal/Invariants/ChildInv.lean
@@ -1,5 +1,5 @@
 import Filrb.Internal.Raw
-
+import Filrb.Internal.Invariants.BST
 /-!
 This module proves that our functionality preserves the `ChildInv` invariant.
 -/
@@ -26,7 +26,54 @@ omit [Preorder α] [Ord α] [LawfulOrd α] in
 @[simp]
 theorem childInv_nil : ChildInv (.nil : Raw α) := ChildInv.nil
 
-theorem childInv_insert_of_bst (x : α) (t : Raw α) (h : ChildInv t) : ChildInv (t.insert x) := sorry
+
+lemma childInv_branch_left_bst {l r : Raw α} (h : ChildInv (.node l d c r)) : ChildInv l := by sorry
+
+lemma childInv_branch_right_bst {l r : Raw α} (h : ChildInv (.node l d c r)) : ChildInv r := by sorry
+
+lemma childInv_paintColor_black_of_bst (t: Raw α) (h: ChildInv t) : ChildInv (t.paintColor .black) := by
+  unfold paintColor
+  split
+  · assumption
+  · apply ChildInv.black
+    apply childInv_branch_left_bst h
+    apply childInv_branch_right_bst h
+
+lemma childInv_baliL_of_bst (tl tr : Raw α) (Hl : ∀ x ∈ tl, x < d) (hl : ChildInv tl)
+    (Hr : ∀ x ∈ tr, d < x) (hr : ChildInv tr) : ChildInv (baliL d tl tl) := sorry
+
+lemma childInv_baliR_of_bst (tl tr : Raw α) (Hl : ∀ x ∈ tl, x < d) (hl : ChildInv tl)
+    (Hr : ∀ x ∈ tr, d < x) (hr : ChildInv tr) : ChildInv (baliR d tl tl) := sorry
+
+lemma childInv_ins_of_bst (d : α) (t : Raw α) (h : ChildInv t) : ChildInv (ins d t) := by
+  unfold ins
+  split
+  · apply ChildInv.red
+    · assumption
+    · sorry
+    · assumption
+    · sorry
+  · split
+    · sorry
+    · sorry
+    · sorry
+  · split
+    · sorry
+    · sorry
+    · sorry
+
+theorem childInv_insert_of_bst (x : α) (t : Raw α) (h : ChildInv t) : ChildInv (t.insert x) := by
+  rcases h with h | h | h
+  · apply ChildInv.black <;> apply ChildInv.nil
+  · unfold insert
+    apply childInv_paintColor_black_of_bst
+    apply childInv_ins_of_bst
+    apply ChildInv.black <;> assumption
+  · unfold insert
+    apply childInv_paintColor_black_of_bst
+    apply childInv_ins_of_bst
+    apply ChildInv.red <;> assumption
+
 theorem childInv_erase_of_bst (x : α) (t : Raw α) (h : ChildInv t) : ChildInv (t.erase x) := sorry
 
 end Raw
